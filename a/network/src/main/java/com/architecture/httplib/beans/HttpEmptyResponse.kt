f0bdf4147e6dd3157401a9eb07719781985a9ec0
Lisa <zenglb@vanke.com>
2024-05-16 17:41:20 +0800

    App 框架搭建，基础库封装，人脸识别

--- a/network/src/main/java/com/architecture/httplib/beans/HttpEmptyResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.architecture.httplib.beans
-
-/**
- * just for test
- *
- */
-class HttpEmptyResponse {
-
-}
--- a/network/src/main/java/com/architecture/httplib/error/BusinessException.kt
+++
@@ -1,9 +0,0 @@
-package com.architecture.httplib.error
-
-import java.lang.RuntimeException
-
-/**
- * 业务的错误，错误码+ 错误String
- *
- */
-class  BusinessException(val code: Int, message: String?) : RuntimeException(message)
